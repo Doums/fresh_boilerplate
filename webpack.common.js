cc9ba4f3d3921c29391c47f5b3e71153528e2086
pierre <dommerc.pierre@gmail.com>
2019-12-23 18:28:38 +0100

    removed @babel/plugin-proposal-class-properties

--- webpack.common.js
+++ webpack.common.js
@@ -39,7 +39,6 @@ module.exports = {
             '@babel/preset-react'
           ],
           plugins: [
-            '@babel/plugin-proposal-class-properties',
             'react-hot-loader/babel'
           ]
         }
